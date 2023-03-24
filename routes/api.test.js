5883a8dd6a1f0501647dad6a0d52ebd0213b39b5
Eliksni <Ipodmaster2000@gmail.com>
2023-03-24 11:07:00 -0700

    Removed supertest from api testing bp since unsure if we need for http testing

--- routes/api.test.js
+++ routes/api.test.js
@@ -1,6 +1,5 @@
 const jest = require('jest');
 const mongoose = require('mongoose');
-const request = require('supertest');
 
 const User = require('../models/User');
 const Server = require('../models/Server');
