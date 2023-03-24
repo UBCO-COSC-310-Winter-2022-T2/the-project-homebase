ebd8ce363add26a7d3e7484a2ca967e630fc369a
Rommel Sabang <sabangrommel@gmail.com>
2023-03-24 16:50:13 -0700

    add register page

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+  
+
 /*-------------- ROUTES --------------*/
 
 
