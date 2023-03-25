d64b9573528dbd3a0c8a9348c90d6b76d4196078
Rommel Sabang <sabangrommel@gmail.com>
2023-03-25 00:20:05 -0700

    first 5 user functionalities

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,8 @@ const User = require('../models/User');
 const Server = require('../models/Server');
 const Message = require('../models/Message');
 
+
+
 // When querying the database for messages, make sure to implement pagination  
 
 module.exports = router
