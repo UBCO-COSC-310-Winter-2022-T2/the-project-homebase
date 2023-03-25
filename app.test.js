58f149b5cec743cc7871982dc9032c37355d9098
Patch <patchfact@gmail.com>
2023-03-25 14:46:03 -0700

    started on testing

--- app.test.js
+++
@@ -1,2 +0,0 @@
-// Don't know if we need anything in here right now as app.js is just boiler plate to get the server up and running
-// Will probably be useful later
--- routes/api.test.js
+++
@@ -1,7 +0,0 @@
-const jest = require('jest');
-const mongoose = require('mongoose');
-
-const User = require('../models/User');
-const Server = require('../models/Server');
-const Message = require('../models/Message');
-
