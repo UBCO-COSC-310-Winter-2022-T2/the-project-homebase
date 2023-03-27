e025ac533037ccf59292bd774e739e5abfe4b781
Eliksni <Ipodmaster2000@gmail.com>
2023-03-26 21:00:11 -0700

    Testing github actions

--- app.test.js
+++ app.test.js
@@ -1,2 +1,6 @@
-// Don't know if we need anything in here right now as app.js is just boiler plate to get the server up and running
-// Will probably be useful later
+describe("first test", () => {
+    it("should be true", () => {
+        expect(true).toBe(true);
+    });
+    }
+);
