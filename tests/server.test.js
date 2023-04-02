a384bfa3034659823eed3396d73439747ecd43dc
Patch <patchfact@gmail.com>
2023-04-01 20:34:57 -0700

    added server join test

---
+++ tests/server.test.js
@@ -0,0 +1,61 @@
+const request = require("supertest");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require("mongoose");
+const app = require("../app.js");
+const User = require("../models/User");
+const Server = require("../models/Server");
+
+let connection;
+
+  beforeAll(async () => {
+    // Connect to a new in-memory database before running any tests
+    mongo = await MongoMemoryServer.create();
+    const uri = mongo.getUri();
+
+    connection = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  afterAll(async () => {
+    // Clear database and close connection
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongo.stop();
+  });
+
+//Joining and leaving a server
+describe("POST /server", () => {
+    
+    it("should join a server", async () => {
+        const user = new User({
+            username: "newuser",
+            email: "newuser@example.com",
+            password: "newpassword",
+            bio: "hello",
+            avatar:
+              "https://cdn1.iconfinder.com/data/icons/basic-ui-set-v5-user-outline/64/Account_profile_user_avatar_small-512.png", //TEMP
+          });
+
+        await user.save();
+
+        const server = new Server({
+            name: "newserver",
+            ownerId: user.id,
+            channels: [
+                {
+                    name: "general",
+                }
+            ],
+            members: [{}] //TEMP
+        });
+
+        await server.save();
+
+        const response = await request(app).post(`/server/${server.name}/join`).send({user});
+
+        expect(response.statusCode).toBe(200);
+        expect(server.members).toContain(user.id);
+    });
+});
