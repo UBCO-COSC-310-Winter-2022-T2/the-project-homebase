3b701cd72a5275f94f327d2d2d42dd3db624322f
Patch <patchfact@gmail.com>
2023-04-02 14:19:21 -0700

    server creation and deletion tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -126,4 +126,107 @@ describe("POST /server", () => {
 
         expect(response.statusCode).toBe(404);
     });
+
+    it("should be able to create a server", async () => {
+        const user = new User({
+            username: "newuser",
+            email: "newuser@gmail.com",
+            password: "newpassword",
+            bio: "hello",
+            avatar: "https://cdn1.iconfinder.com/data/icons/basic-ui-set-v5-user-outline/64/Account_profile_user_avatar_small-512.png", //TEMP
+        });
+        
+        const serverName = "newserver"; 
+        
+        await user.save();
+
+        const response = await request(app)
+            .post("/server/create")
+            .send({ user }, serverName);
+
+        expect(response.statusCode).toBe(200);
+        
+        const server = await Server.findOne({ name: serverName });
+        expect(server).toBeDefined();
+        expect(server.ownerId).toBe(user.id);
+    });
+
+    it("should not be able to create a server with an existing name", async () => {
+        const user = new User({
+            username: "newuser",
+            email: "newuser@gmail.com",
+            password: "newpassword",
+            bio: "hello",
+            avatar: "https://cdn1.iconfinder.com/data/icons/basic-ui-set-v5-user-outline/64/Account_profile_user_avatar_small-512.png", //TEMP
+        });
+
+        const server = new Server({
+            name: "newserver",
+            ownerId: user.id,
+            channels: [
+                {
+                    name: "general",
+                },
+            ],
+            members: [user],
+        });
+
+        await server.save();
+        await user.save();
+
+        const response = await request(app)
+            .post("/server/create")
+            .send({ user }, "newserver");
+        
+        expect(response.statusCode).toBe(400);
+    });
+
+    it("should be able to delete a server", async () => {
+        const user = new User({
+            username: "newuser",
+            email: "newuser@gmail.com",
+            password: "newpassword",
+            bio: "hello",
+            avatar: "https://cdn1.iconfinder.com/data/icons/basic-ui-set-v5-user-outline/64/Account_profile_user_avatar_small-512.png", //TEMP
+        });
+
+        const server = new Server({
+            name: "newserver",
+            ownerId: user.id,
+            channels: [
+                {
+                    name: "general",
+                },
+            ],
+            members: [user],
+        });
+
+        await server.save();
+        await user.save();
+
+        const response = await request(app)
+            .post("/server/delete")
+            .send({ user }, serverName);
+
+        expect(response.statusCode).toBe(200);
+        expect(server).not.toBeDefined();
+    });
+
+    it("should not be able to delete a server that doesn't exist", async () => {
+        const user = new User({
+            username: "newuser",
+            email: "newuser@gmail.com",
+            password: "newpassword",
+            bio: "hello",
+            avatar: "https://cdn1.iconfinder.com/data/icons/basic-ui-set-v5-user-outline/64/Account_profile_user_avatar_small-512.png", //TEMP
+        });
+
+        await user.save();
+
+        const response = await request(app)
+            .post("/server/delete")
+            .send({ user }, "doesntexist");
+        
+        expect(response.statusCode).toBe(404);
+    });
 });
