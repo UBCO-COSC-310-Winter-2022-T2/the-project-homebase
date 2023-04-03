097eca700fea28fd3df6b66a303eedc4d90863b0
jaidyn gordon-mason <jaidynrocks01@gmail.com>
2023-04-03 16:22:04 -0700

    Added join server feature

--- routes/server.js
+++ routes/server.js
@@ -6,7 +6,19 @@ const Server = require("../models/Server");
 const Message = require("../models/Message");
 
 router.get("/join", (req, res) => {
-    res.render('server/join');
+    res.render('server/join', { server: req.name });
   });
+
+router.post("/join", async (req, res) => {
+    const server = await Server.findOne({ server: req.body.name });
+    if (!server) {
+      return res.status(404).send("Server not found");
+    } else {
+        server.members.push(req.user._id);
+        await server.save();
+
+        res.redirect(`/server/${server._id}`);
+    }
+});
 
   module.exports = router;
