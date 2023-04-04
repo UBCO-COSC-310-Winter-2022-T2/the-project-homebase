f4417c61e64fdb33fba4b05111c28bac76f4f479
jaidyn gordon-mason <jaidynrocks01@gmail.com>
2023-04-04 16:11:56 -0700

    Still trying to figure out how to upload a photo

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,35 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { GridFsStorage } = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const mongoose = require("mongoose");
 
 const User = require("../models/User");
 const Server = require("../models/Server");
 const Message = require("../models/Message");
+
+const conn = mongoose.connection;
+conn.once("open", () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+    url: process.env.DATABASE_URL,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        const fileInfo = {
+          filename: file.originalname,
+          bucketName: "uploads"
+        };
+        resolve(fileInfo);
+      });
+    }
+  });
+  const upload = multer({ storage });
 
 router.get("/:id", async (req, res) => {
   const user = await User.findById(req.params.id);
@@ -22,8 +46,8 @@ router.post("/edit/:id", upload.single('avatar'), async (req, res) => {
   let user;
   try {
     user = await User.findById(req.params.id);
-    //TODO - Update user avatar and store url of image in database
-    user.avatar = req.body.avatar;
+   // Update user avatar and store url of image in database
+   user.avatar = `/uploads/${req.file.filename}`; // Update avatar field with file URL
     user.username = req.body.username;
     user.bio = req.body.bio;
     await user.save();
