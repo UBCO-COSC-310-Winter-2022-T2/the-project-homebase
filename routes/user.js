2bb40c3cadd7188cff1d59796290be831664d820
jaidyn gordon-mason <jaidynrocks01@gmail.com>
2023-04-05 15:02:49 -0700

    final commit

--- routes/user.js
+++ routes/user.js
@@ -2,35 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
-const Grid = require("gridfs-stream");
-const mongoose = require("mongoose");
+const upload = multer({ dest: "uploads/" });
 
 const User = require("../models/User");
 const Server = require("../models/Server");
 const Message = require("../models/Message");
-
-const conn = mongoose.connection;
-conn.once("open", () => {
-  // Init stream
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection("uploads");
-});
-
-// Create storage engine
-const storage = new GridFsStorage({
-    url: process.env.DATABASE_URL,
-    file: (req, file) => {
-      return new Promise((resolve, reject) => {
-        const fileInfo = {
-          filename: file.originalname,
-          bucketName: "uploads"
-        };
-        resolve(fileInfo);
-      });
-    }
-  });
-  const upload = multer({ storage });
 
 router.get("/:id", async (req, res) => {
   const user = await User.findById(req.params.id);
@@ -46,12 +22,12 @@ router.post("/edit/:id", upload.single('avatar'), async (req, res) => {
   let user;
   try {
     user = await User.findById(req.params.id);
-   // Update user avatar and store url of image in database
-   user.avatar = `/uploads/${req.file.filename}`; // Update avatar field with file URL
+    //TODO - Update user avatar and store url of image in database
+    user.avatar = req.body.avatar;
     user.username = req.body.username;
     user.bio = req.body.bio;
     await user.save();
-    res.status(200).render("users/userpage", { user: user });
+    res.status(200).render("users/userpage", { user: user }); //TODO - tidy up redirects
   } catch {
     if (user == null) {
       res.status(404).redirect("/");
